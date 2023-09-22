b6db7f8c8cae8a66ec6e1b26f4a253cc084f5876
tomduss <tom.dussaussois@outlook.fr>
2023-09-22 12:10:46 +0200

    start front ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // div for the background image
+    <div className="background_image">
+      <div className="background_login">
+        <div className="login_input">
+          <label for='identifiant'>Identifiant</label>
+          <input id='identifiant' type='text' placeholder='Votre identifiant'></input>
+          <label for='password'>Mot de passe</label>
+          <input id='password' type='password' placeholder='Votre mot de passe'></input>
+          {/* l'input n'a pas envie de se mettre, j'ai une erreur comme quoi le type radio a besoin d'un enfant
+          <input type='radio' name='souvenir'>Se souvenir de moi
+            <label>Se souvenir de moi</label>
+          </input> */}
+          <a href='gfdhjg'>Mot de passe oublié</a>
+          <button className='login_button'>Se connecter</button>
+        </div>
+        <div className="login_information">
+
+        </div>
+      </div>
     </div>
   );
 }
