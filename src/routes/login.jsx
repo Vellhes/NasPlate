27592703f2a8a830179430a79644993d8b8d01db
Vellhes <malobehr@gmail.com>
2023-09-30 16:29:17 +0200

    Création de la page d'inscription

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -6,16 +6,22 @@ export default function Login(){
             <div id="login">
                 <div id="login-form" >
                     <img src={logo} alt="logo" id="logo-login" /><br/>
-                    <label for='identifiant'>Identifiant</label>
-                    <input id='identifiant' type='text' placeholder='Votre identifiant' className="input-text"></input>
-                    <br/>
-                    <br/>
-                    <label for='password'>Mot de passe</label>
-                    <input id='password' type='password' placeholder='Votre mot de passe' className="input-text"></input>
-                    <button className="button-default" id="btn-connexion">Se connecter</button>
-                    <Link to="/register">
-                        Inscrivez-vous
-                    </Link>
+                    <div className='form-item'>
+                        <label for='identifiant'>Identifiant</label>
+                        <input id='identifiant' type='text' placeholder='Votre identifiant' className="input-text"></input>
+                    </div>
+                    <div className='form-item'>
+                        <label for='password'>Mot de passe</label>
+                        <input id='password' type='password' placeholder='Votre mot de passe' className="input-text"></input>
+                    </div>
+                    <div className='form-item'>
+                        <button className="button-default" id="btn-connexion">Se connecter</button>
+                    </div>
+                    <div className='form-item'>
+                        <Link to="/register">
+                            Inscrivez-vous
+                        </Link>
+                    </div>
                 </div>
                 <div id="login-side">
                     <h2>Bienvenue dans votre espace NasPlate</h2>
--- src/routes/register.jsx
+++ src/routes/register.jsx
@@ -4,20 +4,39 @@ export default function Register(){
         <div id="body-form-register">
             <div id="register">
                 <div id="register-form">
+                    <div className="two-columns">
+                        <div className="form-item">
+                            <label for='prenom'>Nom</label>
+                            <input id='prenom' type='text' placeholder='Votre nom' className="input-text"></input>
+                        </div>
+                        <div className="form-item">
+                            <label for='prenom'>Prénom</label>
+                            <input id='prenom' type='text' placeholder='Votre prénom' className="input-text"></input>
+                        </div>
+                    </div>
+                    <div className="form-item">
+                        <label for='mail'>Adresse mail</label>
+                        <input id='mail' type='email' placeholder='Votre adresse mail' className="input-text"></input>
+                    </div>
+                    <div className="form-item">
+                        <label for='password'>Mot de passe</label>
+                        <input id='password' type='password' placeholder='Votre adresse mail' className="input-text"></input>
+                    </div>
+                    <div className="form-item">
+                        <label for='validatePassword'>Valider le mot de passe</label>
+                        <input id='validatePassword' type='password' placeholder='Votre adresse mail' className="input-text"></input>
+                    </div>
+                    <div className="form-item">
+                        <button className="button-default" id="btn-inscription">Valider l'inscription</button>
+                    </div>
+                </div>
+                <div id="register-side">
                     <div id="div-logo">
                         <img src={logo} alt="logo" id="logo-register" /><br/>
                     </div>
-                    <div className="flex-rows">
-                        <div className="col-50">
-                            <label for='prenom'>Nom</label>
-                            <input id='prenom' type='text' placeholder='Votre identifiant' className="input-text"></input>
-                        </div>
-                        <div className="col-50">
-                            <label for='prenom'>Nom</label>
-                            <input id='prenom' type='text' placeholder='Votre identifiant' className="input-text"></input>
-                        </div>
-                    </div>
-                    
+                    <h2>
+                        NasPlate - Inscription
+                    </h2>
                 </div>
             </div>
         </div>
