f3c44e5612e5125f3d587b7fe1a319339f75c2a0
Vellhes <malobehr@gmail.com>
2023-10-06 12:12:22 +0200

    V1 de la page de connexion finie

--- src/routes/register.jsx
+++ src/routes/register.jsx
@@ -1,4 +1,5 @@
 import logo from '../img/nasplate.png';
+import {Link} from "react-router-dom";
 export default function Register(){
     return(
         <div id="body-form-register">
@@ -28,6 +29,11 @@ export default function Register(){
                     </div>
                     <div className="form-item">
                         <button className="button-default" id="btn-inscription">Valider l'inscription</button>
+                    </div>
+                    <div className='form-item'>
+                        <Link to="/login">
+                            Connectez-vous
+                        </Link>
                     </div>
                 </div>
                 <div id="register-side">
